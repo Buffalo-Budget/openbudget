6fc40a1cb1c14d9a57480133827997cba4f4a85c
Austin Audits <austinaudits@opensourcebuffalo.com>
2025-10-03 21:22:04 -0400

    Create api-openbook.js
    the legacy api url's for the open book buffalo site.  api's include the most useful budget information revenues, expenses, payroll, and checkbook

---
+++ legacy/api-openbook.js
@@ -0,0 +1,4 @@
+const apiCheckbook = "https://data.buffalony.gov/resource/bktd-jwim.json";
+const apiPayroll = "https://data.buffalony.gov/resource/hm3x-8br6.json";
+const apiExpense = "https://data.buffalony.gov/resource/xy5k-883e.json";
+const apiRevenue = "https://data.buffalony.gov/resource/cvx5-9drv.json";
